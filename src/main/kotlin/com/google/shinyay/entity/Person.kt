c875084a5890e0bc3d321ab6e74b5d41775b4a45
shinyay <shinya.com@gmail.com>
2021-03-03 16:48:36 +0900

    Add coulums for person

--- src/main/kotlin/com/google/shinyay/entity/Person.kt
+++ src/main/kotlin/com/google/shinyay/entity/Person.kt
@@ -2,5 +2,7 @@ package com.google.shinyay.entity
 
 data class Person(
     val firstName: String,
-    val lastName: String
+    val lastName: String,
+    val email: String,
+    val location: String
 )
