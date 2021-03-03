b0e7e4724ca98a7895acd91a606bef8735ff64a4
shinyay <shinya.com@gmail.com>
2021-03-03 18:02:55 +0900

    Add reader function

--- src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
@@ -1,7 +1,19 @@
 package com.google.shinyay.configuration
 
+import com.google.shinyay.entity.Person
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class BatchConfiguration {
+class BatchConfiguration(
+    val jobBuilderFactory: JobBuilderFactory,
+    val stepBuilderFactory: StepBuilderFactory
+) {
+    @Bean
+    fun reader(): FlatFileItemReader<Person> = FlatFileItemReaderBuilder<Person>()
+        .build()
 }
