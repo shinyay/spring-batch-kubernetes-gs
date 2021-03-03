a3972f5b8e9f90949a799b4abaf7fb691940faa9
shinyay <shinya.com@gmail.com>
2021-03-03 21:27:50 +0900

    Add jobParameter with SpEL

--- src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
@@ -3,17 +3,27 @@ package com.google.shinyay.configuration
 import com.google.shinyay.entity.Person
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.file.FlatFileItemReader
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
+import org.springframework.core.io.UrlResource
 
 @Configuration
 class BatchConfiguration(
     val jobBuilderFactory: JobBuilderFactory,
     val stepBuilderFactory: StepBuilderFactory
 ) {
+
+    @Bean
+    @StepScope
+    fun resource(@Value("#{jobParameters['fileName']}") fileName: String): Resource = UrlResource(fileName)
+
     @Bean
     fun reader(): FlatFileItemReader<Person> = FlatFileItemReaderBuilder<Person>()
+        .name("PersonReader")
         .build()
 }
