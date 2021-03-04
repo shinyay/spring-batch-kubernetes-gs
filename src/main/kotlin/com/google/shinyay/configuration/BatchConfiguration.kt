c3ff7590dd7ff0af44eaa35c2c32f00e86fddbd2
shinyay <shinya.com@gmail.com>
2021-03-04 14:30:09 +0900

    Add step function snipet

--- src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay.configuration
 
 import com.google.shinyay.entity.Person
+import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepScope
@@ -47,5 +48,10 @@ class BatchConfiguration(
         .beanMapped()
         .sql("INSERT INTO people (first_name, last_name, email, location) VALUES (:firstName, :lastName. :email, :location)")
         .dataSource(dataSource)
+        .build()
+
+    @Bean
+    fun step(): Step = stepBuilderFactory.get("uppercase-step")
+        .chunk<Person, Person>(10)
         .build()
 }
