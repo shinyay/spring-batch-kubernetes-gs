9bf2ce1a102cd15d1684fd07cf847a19e0a7ac54
shinyay <shinya.com@gmail.com>
2021-03-06 13:28:23 +0900

    Add dependency for Spring Cloud GCP for MySQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,22 @@ repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "Hoxton.SR8"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.cloud:spring-cloud-gcp-starter-sql-mysql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.batch:spring-batch-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
