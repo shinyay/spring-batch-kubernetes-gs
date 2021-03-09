1b51c72dff835014d385ff9bd845636e1a6e5434
shinyay <shinya.com@gmail.com>
2021-03-09 12:58:01 +0900

    Add age parameter for Entity

--- src/main/kotlin/com/google/shinyay/entity/Person.kt
+++ src/main/kotlin/com/google/shinyay/entity/Person.kt
@@ -3,6 +3,7 @@ package com.google.shinyay.entity
 data class Person(
     var firstName: String = "",
     var lastName: String = "",
+    var age: Int = 0,
     var email: String = "",
     var location: String = ""
 )
